27740bfebf83335fdc78e0b36a06da4ce66d2b98
Oscar Moreno <os.kardavid70@gmail.com>
2020-03-16 17:05:48 -0600

    update

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use('/user', userRoutes)
-app.use('/plant', plantRoutes)
+app.use('/plant', plantRoutes) 
 
 
 app.listen(PORT, () => {
