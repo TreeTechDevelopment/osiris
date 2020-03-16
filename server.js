3e8c6a6bec4eb4738fa038dd752eaff3045d08a5
Oscar Moreno <os.kardavid70@gmail.com>
2020-03-16 16:51:38 -0600

    update

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-const server = require('http').createServer(app);
-
 require('./src/db/db');
 const userRoutes = require('./src/routes/userRoutes');
 const plantRoutes = require('./src/routes/plantRoutes');
 
-const PORT = 3000 || process.env.PORT
+const PORT = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -17,6 +15,6 @@ app.use('/user', userRoutes)
 app.use('/plant', plantRoutes)
 
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`server on port ${PORT}`)
 })   
