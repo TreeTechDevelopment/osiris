4e52072d01e580b0af3899a22344f41fdd5522e9
oscarMG11 <os.kardavid70@gmail.com>
2020-05-28 20:03:37 -0500

    update

--- src/schedules.js
+++ src/schedules.js
@@ -30,30 +30,32 @@ const jobCheckEmployeeDone = new CronJob('0 0 3 */1 * *', async () => {
             let plantTo = numberToSerialNumber(employees[i].plants.split('-')[1])
 
             let section = await sectionCollection.findOne({ 'sectionName': employees[i].section })
-            let dateCheckFrom = section.checkDateFrom
-            let dateCheckTo = section.checkDateTo
+            if(section){
+                let dateCheckFrom = section.checkDateFrom
+                let dateCheckTo = section.checkDateTo
 
-            let plants = await plantCollection.find({ 'serialNumber': { $gte: plantFrom, $lte: plantTo } })
+                let plants = await plantCollection.find({ 'serialNumber': { $gte: plantFrom, $lte: plantTo } })
 
-            let missingPlants = []
+                let missingPlants = []
 
-            for(let j = 0; j < plants.length; j++){
-                if(plants[j].lastUpdate){
-                    if(!checkDate(plants[j].lastUpdate, dateCheckFrom, dateCheckTo)){
+                for(let j = 0; j < plants.length; j++){
+                    if(plants[j].lastUpdate){
+                        if(!checkDate(plants[j].lastUpdate, dateCheckFrom, dateCheckTo)){
+                            missingPlants.push(plants[j].serialNumber)
+                        }
+                    }else{
                         missingPlants.push(plants[j].serialNumber)
                     }
-                }else{
-                    missingPlants.push(plants[j].serialNumber)
                 }
-            }
 
-            if(missingPlants.length === 0){ 
-                section.finishRead = true
-                section.save()
+                if(missingPlants.length === 0){ 
+                    section.finishRead = true
+                    section.save()
+                }
+                
+                employees[i].missingPlants = missingPlantsFormatted(missingPlants)
+                employees[i].save()
             }
-            
-            employees[i].missingPlants = missingPlantsFormatted(missingPlants)
-            employees[i].save()
         }
     }catch(e){ console.log(e) }
 });
@@ -65,7 +67,7 @@ const jobCheckChat = new CronJob('0 10 3 */1 * *', async () => {
 
 const jobCheckDate = new CronJob('0 20 3 */1 * *', async () => {
     const sections = await sectionCollection.find({})
-    for(let i = 0; i < section.length; i++){
+    for(let i = 0; i < sections.length; i++){
         const newDateFrom = moment(moment(sections[i].dateCheckTo, 'DD/MM/YYYY').add(1, 'day').toDate()).format('DD/MM/YYYY')
         if(!checkDate(moment(moment().toDate()).format('DD/MM/YYYY'), sections[i].dateCheckFrom, newDateFrom)){            
             let date = moment(newDateFrom, 'DD/MM/YYYY').add(28, 'day').toDate()
